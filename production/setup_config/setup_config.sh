06b188c824d0d75995e633c5e8567a63704f7b5d
dvir1994 <33489902+dvir1994@users.noreply.github.com>
2022-09-08 17:57:54 +0800

    Update setup_config.sh

--- production/setup_config/setup_config.sh
+++ production/setup_config/setup_config.sh
@@ -18,7 +18,7 @@ nginx_username=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 42 | head -n 1)
 secret_password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 42 | head -n 1)
 
 # Blockchain
-seed_node="4ad8e40126f5d645786e56b1ce3b69a3bcd09811@prod-pnet-seed-node.lavanet.xyz:26656"
+seed_node="04d6301d7f3681e3cf97e77b244562cfceaadbd0@prod-pnet-seed-node.lavanet.xyz:26656"
 
 # Lavad service
 keyring_backend="test"
