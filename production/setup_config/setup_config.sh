b632a898b045fa71dd0b9b53de3aef8381a20473
Dvir Chikvashvili <dvir211994@gmail.com>
2022-10-31 00:52:31 +0400

    Updated network reset

--- production/setup_config/setup_config.sh
+++ production/setup_config/setup_config.sh
@@ -18,7 +18,7 @@ nginx_username=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 42 | head -n 1)
 secret_password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 42 | head -n 1)
 
 # Blockchain
-seed_node="95b1d35492b8e432fb61a3bbdaa16bce8a4c3e59@prod-pnet-seed-node.lavanet.xyz:26656"
+seed_node="558bb9cedae211161ee962f6a82d100984793299@prod-pnet-seed-node.lavanet.xyz:26656"
 
 # Lavad service
 keyring_backend="test"
